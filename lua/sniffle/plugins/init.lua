4a05409b007aeb582790747f077751fd18bef997
sniffle <areveldejensen@gmail.com>
2024-01-09 22:17:46 +0100

    indentation because I'm an animal

--- lua/sniffle/plugins/init.lua
+++ lua/sniffle/plugins/init.lua
@@ -1,18 +1,17 @@
 return {
- "nvim-lua/plenary.nvim",
-  {
+    "nvim-lua/plenary.nvim",
+    {
         "rose-pine/neovim",
         name = "rose-pine",
         config = function()
             vim.cmd("colorscheme rose-pine")
         end
-  },
-  {
-            "saecki/crates.nvim",
-            config = function()
-                require('crates').setup()
-     	    end,
-  },
-  "nvim-treesitter/nvim-treesitter-context",
-
+    },
+    {
+        "saecki/crates.nvim",
+        config = function()
+            require('crates').setup()
+        end,
+    },
+    "nvim-treesitter/nvim-treesitter-context",
 }
