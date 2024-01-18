f2e12db407a66f1c5d8cd5f2159a331ac9cae1b8
Are Velde Jensen <are.velde.jensen@nrk.no>
2024-01-18 09:26:21 +0100

    telescope change to live grep and buffer shortcut

--- lua/sniffle/plugins/telescope.lua
+++ lua/sniffle/plugins/telescope.lua
@@ -15,9 +15,8 @@ return {
             local word = vim.fn.expand("<cword>")
             builtin.grep_string({ search = word});
         end)
-        vim.keymap.set('n', '<leader>ps', function()
-            builtin.grep_string({ search = vim.fn.input("Grep > ")});
-        end)
+        vim.keymap.set('n', '<leader>ps', builtin.live_grep, {})
         vim.keymap.set('n', '<leader>vh', builtin.help_tags, {})
+        vim.keymap.set('n', '<leader>pb', builtin.buffers, {})
     end
 }
