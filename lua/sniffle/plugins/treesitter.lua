38a69385de29a67beba76a74faa51f42fc5b30f1
Are Velde Jensen <are.velde.jensen@nrk.no>
2024-02-22 11:54:09 +0100

    comment out ignore install js

--- lua/sniffle/plugins/treesitter.lua
+++ lua/sniffle/plugins/treesitter.lua
@@ -2,34 +2,34 @@ return {
     "nvim-treesitter/nvim-treesitter",
     build = ":TSUpdate",
     config =
-    function()
-        require 'nvim-treesitter.configs'.setup {
-            -- A list of parser names, or "all" (the five listed parsers should always be installed)
-            ensure_installed = { "c", "lua", "vim", "vimdoc", "query", "rust", "toml", "css", "html" },
+        function()
+            require 'nvim-treesitter.configs'.setup {
+                -- A list of parser names, or "all" (the five listed parsers should always be installed)
+                ensure_installed = { "c", "lua", "vim", "vimdoc", "query", "rust", "toml", "css", "html" },
 
-            -- Install parsers synchronously (only applied to `ensure_installed`)
-            sync_install = false,
+                -- Install parsers synchronously (only applied to `ensure_installed`)
+                sync_install = false,
 
-            -- Automatically install missing parsers when entering buffer
-            -- Recommendation: set to false if you don't have `tree-sitter` CLI installed locally
-            auto_install = true,
+                -- Automatically install missing parsers when entering buffer
+                -- Recommendation: set to false if you don't have `tree-sitter` CLI installed locally
+                auto_install = true,
 
-            -- List of parsers to ignore installing (for "all")
-            ignore_install = { "javascript" },
+                -- List of parsers to ignore installing (for "all")
+                ignore_install = { "javascript" },
 
-            ---- If you need to change the installation directory of the parsers (see -> Advanced Setup)
-            -- parser_install_dir = "/some/path/to/store/parsers", -- Remember to run vim.opt.runtimepath:append("/some/path/to/store/parsers")!
+                ---- If you need to change the installation directory of the parsers (see -> Advanced Setup)
+                -- parser_install_dir = "/some/path/to/store/parsers", -- Remember to run vim.opt.runtimepath:append("/some/path/to/store/parsers")!
 
-            highlight = {
-                enable = true,
+                highlight = {
+                    enable = true,
 
-                -- NOTE: these are the names of the parsers and not the filetype. (for example if you want to
-                -- disable highlighting for the `tex` filetype, you need to include `latex` in this list as this is
-                -- the name of the parser)
-                -- list of language that will be disabled
-                -- disable = { "c", "rust" },
-                -- Or use a function for more flexibility, e.g. to disable slow treesitter highlight for large files
-                --    disable = function(lang, buf)
+                    -- NOTE: these are the names of the parsers and not the filetype. (for example if you want to
+                    -- disable highlighting for the `tex` filetype, you need to include `latex` in this list as this is
+                    -- the name of the parser)
+                    -- list of language that will be disabled
+                    -- disable = { "c", "rust" },
+                    -- Or use a function for more flexibility, e.g. to disable slow treesitter highlight for large files
+                    --    disable = function(lang, buf)
                     --	    local max_filesize = 100 * 1024 -- 100 KB
                     --	    local ok, stats = pcall(vim.loop.fs_stat, vim.api.nvim_buf_get_name(buf))
                     --	    if ok and stats and stats.size > max_filesize then
@@ -51,4 +51,4 @@ return {
                 },
             }
         end
-    }
+}
