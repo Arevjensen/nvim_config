64707ee44577eb97d406f610dbc74b62f94079b5
Are Velde Jensen <areveldejensen@gmail.com>
2024-06-19 20:14:29 +0200

    New theme and lualine

--- lua/sniffle/plugins/init.lua
+++ lua/sniffle/plugins/init.lua
@@ -2,10 +2,18 @@ return {
     "nvim-lua/plenary.nvim",
     {
         "rose-pine/neovim",
+        -- priority = 1000,
+        -- name = "rose-pine",
+        -- config = function()
+        --     vim.cmd("colorscheme rose-pine")
+        -- end
+    },
+    {
+        "EdenEast/nightfox.nvim",
         priority = 1000,
-        name = "rose-pine",
+        name = "nightfox",
         config = function()
-            vim.cmd("colorscheme rose-pine")
+            vim.cmd("colorscheme nightfox")
         end
     },
     {
