97ed879550a3d444ee2a5447eaa0c25323427695
Are Velde Jensen <are.velde.jensen@nrk.no>
2024-09-12 09:19:32 +0200

    added markdown support and updated plugins

--- lua/sniffle/plugins/lsp.lua
+++ lua/sniffle/plugins/lsp.lua
@@ -51,7 +51,8 @@ return {
                 htmx = {},
                 eslint = {},
                 html = {},
-                cssls = {}
+                cssls = {},
+                marksman = {},
             }
             local capabilities = require('cmp_nvim_lsp').default_capabilities(vim.lsp.protocol.make_client_capabilities())
 
