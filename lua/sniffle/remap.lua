83a32fb523379c783fd0dbb4efc7a6be3aa8a0d9
Are Velde Jensen <are.velde.jensen@nrk.no>
2024-10-02 09:50:49 +0200

    removed whitespace

--- lua/sniffle/remap.lua
+++ lua/sniffle/remap.lua
@@ -2,7 +2,6 @@ vim.keymap.set({ 'n', 'v' }, '<Space>', '<Nop>', { silent = true })
 vim.g.mapleader = ' '
 vim.g.maplocalleader = ' '
 
-
 vim.keymap.set("v", "J", ":m '>+1<CR>gv=gv")
 vim.keymap.set("v", "K", ":m '<-2<CR>gv=gv")
 
