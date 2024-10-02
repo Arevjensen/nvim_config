e92b9b5161aafc8d3e90675f644c2efc09167c4a
Are Velde Jensen <are.velde.jensen@nrk.no>
2024-10-02 09:51:04 +0200

    load event on surround

--- lua/sniffle/plugins/mini.lua
+++ lua/sniffle/plugins/mini.lua
@@ -1,6 +1,7 @@
 return {
     {
         "echasnovski/mini.surround",
+        event = 'InsertEnter',
         keys = function(_, keys)
             -- Populate the keys based on the user's options
             local plugin = require("lazy.core.config").spec.plugins["mini.surround"]
