8f46d7e82d413a62945608ed85441b0d41610ebc
Are Velde Jensen <are.velde.jensen@nrk.no>
2024-10-29 12:58:24 +0100

    Updated harpoon to v2

--- lua/sniffle/plugins/harpoon.lua
+++ lua/sniffle/plugins/harpoon.lua
@@ -1,21 +1,21 @@
 return {
-    'ThePrimeagen/harpoon',
-    dependencies = 'nvim-lua/plenary.nvim',
-    config = function()
-        local harpoon = require('harpoon')
-        harpoon.setup {}
-        local harpoon_mark = require('harpoon.mark')
-        local harpoon_ui = require('harpoon.ui')
-        local harpoon_term = require('harpoon.term')
-        vim.keymap.set('n', '<M-a>', function() harpoon_mark.add_file() end, {})
-        vim.keymap.set('n', '<M-n>', function() harpoon_ui.nav_next() end, {})
-        vim.keymap.set('n', '<M-p>', function() harpoon_ui.nav_prev() end, {})
-        vim.keymap.set('n', '<M-q>', function() harpoon_ui.toggle_quick_menu() end, {})
-        vim.keymap.set('n', '<M-1>', function() harpoon_ui.nav_file(1) end, {})
-        vim.keymap.set('n', '<M-2>', function() harpoon_ui.nav_file(2) end, {})
-        vim.keymap.set('n', '<M-3>', function() harpoon_ui.nav_file(3) end, {})
-        vim.keymap.set('n', '<M-4>', function() harpoon_ui.nav_file(4) end, {})
-        vim.keymap.set('n', '<M-t>', function() harpoon_term.gotoTerminal(1) end, {})
-    end
+	'ThePrimeagen/harpoon',
+	branch = "harpoon2",
+	dependencies = 'nvim-lua/plenary.nvim',
+	config = function()
+		local harpoon = require("harpoon")
+		harpoon:setup()
+
+		vim.keymap.set("n", "<M-a>", function() harpoon:list():add() end)
+		vim.keymap.set("n", "<M-q>", function() harpoon.ui:toggle_quick_menu(harpoon:list()) end)
+
+		vim.keymap.set("n", "<M-1>", function() harpoon:list():select(1) end)
+		vim.keymap.set("n", "<M-2>", function() harpoon:list():select(2) end)
+		vim.keymap.set("n", "<M-3>", function() harpoon:list():select(3) end)
+		vim.keymap.set("n", "<M-4>", function() harpoon:list():select(4) end)
+
+		vim.keymap.set("n", "<M-p>", function() harpoon:list():prev() end)
+		vim.keymap.set("n", "<M-n>", function() harpoon:list():next() end)
+	end,
 
 }
--- lua/sniffle/plugins/lsp.lua
+++ lua/sniffle/plugins/lsp.lua
@@ -1,103 +1,104 @@
 local function on_attach_keys(opts)
-    vim.keymap.set("n", "gd", function() vim.lsp.buf.definition() end, opts)
-    vim.keymap.set("n", "gD", function() vim.lsp.buf.declaration() end, opts)
-    vim.keymap.set("n", "gi", function() vim.lsp.buf.implementation() end, opts)
-    vim.keymap.set("n", "go", function() vim.lsp.buf.type_definition() end, opts)
-    vim.keymap.set("n", "K", function() vim.lsp.buf.hover() end, opts)
-    vim.keymap.set("n", "<leader>vws", function() vim.lsp.buf.workspace_symbol() end, opts)
-    vim.keymap.set("n", "<leader>vd", function() vim.diagnostic.open_float() end, opts)
-    vim.keymap.set("n", "<leader>ca", function() vim.lsp.buf.code_action() end, opts)
-    vim.keymap.set("n", "<leader>vrr", function() vim.lsp.buf.references() end, opts)
-    vim.keymap.set("n", "<leader>vrn", function() vim.lsp.buf.rename() end, opts)
-    vim.keymap.set("n", "<leader>ct", function() vim.lsp.inlay_hint.enable(not vim.lsp.inlay_hint.is_enabled()) end)
-    vim.keymap.set("n", "<leader>cf", function() vim.lsp.buf.format() end, opts)
-    vim.keymap.set("i", "<C-h>", function() vim.lsp.buf.signature_help() end, opts)
+	vim.keymap.set("n", "gd", function() vim.lsp.buf.definition() end, opts)
+	vim.keymap.set("n", "gD", function() vim.lsp.buf.declaration() end, opts)
+	vim.keymap.set("n", "gi", function() vim.lsp.buf.implementation() end, opts)
+	vim.keymap.set("n", "go", function() vim.lsp.buf.type_definition() end, opts)
+	vim.keymap.set("n", "K", function() vim.lsp.buf.hover() end, opts)
+	vim.keymap.set("n", "<leader>vws", function() vim.lsp.buf.workspace_symbol() end, opts)
+	vim.keymap.set("n", "<leader>vd", function() vim.diagnostic.open_float() end, opts)
+	vim.keymap.set("n", "<leader>ca", function() vim.lsp.buf.code_action() end, opts)
+	vim.keymap.set("n", "<leader>vrr", function() vim.lsp.buf.references() end, opts)
+	vim.keymap.set("n", "<leader>vrn", function() vim.lsp.buf.rename() end, opts)
+	vim.keymap.set("n", "<leader>ct", function() vim.lsp.inlay_hint.enable(not vim.lsp.inlay_hint.is_enabled()) end)
+	vim.keymap.set("n", "<leader>cf", function() vim.lsp.buf.format() end, opts)
+	vim.keymap.set("i", "<C-h>", function() vim.lsp.buf.signature_help() end, opts)
 end
 
 return {
-    {
-        "neovim/nvim-lspconfig",
-        event = { "BufReadPost", "BufWritePost", "BufNewFile" },
-        dependencies = {
-            {
-                "williamboman/mason.nvim",
-                build = function()
-                    pcall(vim.cmd, "MasonUpdate")
-                end,
-            },
-            "williamboman/mason-lspconfig.nvim",
-            'WhoIsSethDaniel/mason-tool-installer.nvim',
-            'folke/neodev.nvim',
+	{
+		"neovim/nvim-lspconfig",
+		event = { "BufReadPost", "BufWritePost", "BufNewFile" },
+		dependencies = {
+			{
+				"williamboman/mason.nvim",
+				build = function()
+					pcall(vim.cmd, "MasonUpdate")
+				end,
+			},
+			"williamboman/mason-lspconfig.nvim",
+			'WhoIsSethDaniel/mason-tool-installer.nvim',
+			'folke/neodev.nvim',
 
-            { 'j-hui/fidget.nvim', opts = {} }
-        },
-        opts = { inlay_hints = { enabled = true }, },
-        config = function()
-            local servers = {
-                rust_analyzer = {
-                    ["rust-analyzer"] = {
-                        checkOnSave = {
-                            command = "clippy",
-                        },
-                    },
-                },
-                lua_ls = {
-                    Lua = {
-                        workspace = { checkThirdParty = false },
-                        telemetry = { enable = false },
-                    },
-                },
-                tailwindcss = {},
-                htmx = {},
-                eslint = {},
-                html = {},
-                cssls = {},
-                marksman = {},
-            }
-            local capabilities = require('cmp_nvim_lsp').default_capabilities(vim.lsp.protocol.make_client_capabilities())
+			{ 'j-hui/fidget.nvim', opts = {} }
+		},
+		opts = { inlay_hints = { enabled = true }, },
+		config = function()
+			local servers = {
+				rust_analyzer = {
+					["rust-analyzer"] = {
+						checkOnSave = {
+							command = "clippy",
+						},
+					},
+				},
+				lua_ls = {
+					Lua = {
+						workspace = { checkThirdParty = false },
+						telemetry = { enable = false },
+					},
+				},
+				tailwindcss = {},
+				htmx = {},
+				eslint = {},
+				html = {},
+				cssls = {},
+				marksman = {},
+			}
+			local capabilities = require('cmp_nvim_lsp').default_capabilities(vim.lsp.protocol
+				.make_client_capabilities())
 
-            require('neodev').setup()
-            require("mason").setup()
-            local mason_lspconfig = require 'mason-lspconfig'
+			require('neodev').setup()
+			require("mason").setup()
+			local mason_lspconfig = require 'mason-lspconfig'
 
-            mason_lspconfig.setup {
-                ensure_installed = vim.tbl_keys(servers),
-                ui = {
-                    icons = {
-                        package_installed = "✓",
-                        package_pending = "➜",
-                        package_uninstalled = "✗"
-                    }
-                },
-            }
+			mason_lspconfig.setup {
+				ensure_installed = vim.tbl_keys(servers),
+				ui = {
+					icons = {
+						package_installed = "✓",
+						package_pending = "➜",
+						package_uninstalled = "✗"
+					}
+				},
+			}
 
-            mason_lspconfig.setup_handlers {
-                function(server_name)
-                    require('lspconfig')[server_name].setup {
-                        capabilities = capabilities,
-                        settings = servers[server_name],
-                    }
-                end,
-            }
-            vim.api.nvim_create_autocmd('LspAttach', {
-                group = vim.api.nvim_create_augroup('UserLspConfig', {}),
-                callback = function(ev)
-                    local opts = { buffer = ev.buf }
-                    on_attach_keys(opts)
-                end,
-            })
+			mason_lspconfig.setup_handlers {
+				function(server_name)
+					require('lspconfig')[server_name].setup {
+						capabilities = capabilities,
+						settings = servers[server_name],
+					}
+				end,
+			}
+			vim.api.nvim_create_autocmd('LspAttach', {
+				group = vim.api.nvim_create_augroup('UserLspConfig', {}),
+				callback = function(ev)
+					local opts = { buffer = ev.buf }
+					on_attach_keys(opts)
+				end,
+			})
 
-            vim.diagnostic.config({
-                update_in_insert = true,
-                float = {
-                    focusable = false,
-                    style = "minimal",
-                    border = "rounded",
-                    source = true,
-                    header = "",
-                    prefix = ""
-                }
-            })
-        end
-    }
+			vim.diagnostic.config({
+				update_in_insert = true,
+				float = {
+					focusable = false,
+					style = "minimal",
+					border = "rounded",
+					source = true,
+					header = "",
+					prefix = ""
+				}
+			})
+		end
+	}
 }
